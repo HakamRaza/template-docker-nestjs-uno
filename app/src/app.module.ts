6775ac61b6b0d083ed57d01fefddfb550bd9e9e6
HakamRaza <ismatcd@yahoo.com>
2024-01-03 19:07:46 +0800

    feat: add transactional helper

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RouterModule } from '@nestjs/core';
+
+// Other dependencies
+import { DataSource } from 'typeorm';
+import { addTransactionalDataSource, getDataSourceByName } from 'typeorm-transactional';
+
+// Local files
+import { versionRoutes } from 'src/version.routes';
+import { configService } from 'src/shared/Services/config.service';
+import { V1Module } from './v1/v1.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRootAsync({
+			useFactory: () => configService.getTypeOrmConfig(),
+			dataSourceFactory: async (options) => {
+				if (!options) throw new Error('Invalid options passed');
+				return getDataSourceByName('default') || addTransactionalDataSource(new DataSource(options));
+			  },
+		}),
+		RouterModule.register(versionRoutes),
+		V1Module,
+	]
 })
 export class AppModule {}
